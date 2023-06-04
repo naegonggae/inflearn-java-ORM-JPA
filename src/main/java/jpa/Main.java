53835babce25500ac04be9faa1204dc97555ab34
naegonggae <tkdtkd97@naver.com>
2023-06-04 23:25:57 +0900

    :memo: 객체와 테이블을 생성하고 매핑하기 - 회원 수정

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -10,7 +10,12 @@ public class Main {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		// persistence.xml 에 있는 Persistence unit name = hello
+		// EntityManagerFactory 는 웹이 실행될 때 딱 하나만 생성이 됨
 		EntityManager entityManager = emf.createEntityManager();
+		// EntityManager 는 고객 요청이 올때마다 썼다가 지웠다가 함
+		// 쓰레드간 공유 X, 사용하고 버려야함
+		// JPA 의 모든 변경은 트랜잭션 내부에서 실행되어야한다.
+
 		// 실제 동작 코드 작성
 
 		EntityTransaction tx = entityManager.getTransaction();
@@ -19,13 +24,11 @@ public class Main {
 		try {
 			// 정상적인 흐름
 
-			// 조회
-			Member findMember = entityManager.find(Member.class, 1L);
-			System.out.println("findMember.id = " + findMember.getId());
-			System.out.println("findMember.name = " + findMember.getName());
-
-			// 삭제
-			entityManager.remove(findMember);
+			// 수정
+			Member findMember = entityManager.find(Member.class, 2L);
+			findMember.setName("JPA");
+			// 왜 수정만 하고 저장하지 않았는데 수정이 정상적으로 될까?
+			// jpa 가 뭔가 수정을 발견하면 커밋 날리기 전에 update 쿼리를 날린다.
 
 			tx.commit(); // 트랜잭션 종료
 		} catch (Exception e) {
