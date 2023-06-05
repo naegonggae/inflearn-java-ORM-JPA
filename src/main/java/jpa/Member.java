bfa5dc0e3e6df63dcb4dc9cbed44fd41b2d25079
naegonggae <tkdtkd97@naver.com>
2023-06-05 17:39:14 +0900

    :memo: 데이터베이스 스키마 자동 생성 - DDL

--- src/main/java/jpa/Member.java
+++ src/main/java/jpa/Member.java
@@ -5,13 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "Member") // 이름 지정가능
+@Entity(name = "Member") // 다른 이름 지정가능
+@Table(name = "Member") // 이름 지정가능 , 엔티티와 메핑할 테이블 지정
 public class Member {
 
 	@Id
 	private Long id;
-	@Column(name = "name") // 이름 지정가능
+	@Column(name = "name", unique = true, nullable = false, length = 10) // 이름 지정가능
 	private String name;
 
 	public Member() { // 기본 생성자는 무조건 필요하다
