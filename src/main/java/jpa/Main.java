346ad1f6433f50f7c4e0776e9a7760e989838131
naegonggae <tkdtkd97@naver.com>
2023-06-08 16:50:33 +0900

    :memo: 프록시 비교

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -18,15 +18,23 @@ public class Main {
 
 		try {
 
-			Member member = new Member();
-			member.setUsername("userA");
-			member.setCreatedBy("kim");
-			member.setCreatedDate(LocalDateTime.now());
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			entityManager.persist(member1);
 
-			entityManager.persist(member);
+			Member member2 = new Member();
+			member2.setUsername("member2");
+			entityManager.persist(member2);
 
 			entityManager.flush();
 			entityManager.clear();
+
+			Member m1 = entityManager.find(Member.class, member1.getId());
+			Member m2 = entityManager.getReference(Member.class, member2.getId());
+			System.out.println("m1 + m2 = " + (m1.getClass() == m2.getClass())); // 다르다
+			// 파일 비교는 == 으로 하면 안됨
+			System.out.println("m1 + m2 = " + (m1 instanceof Member)); // 클래스를 비교할때는 이렇게 해야함
+			System.out.println("m1 + m2 = " + (m2 instanceof Member));
 
 			tx.commit(); // 트랜잭션 종료 // 임시 저장했던 쿼리를 실제로 날린다.
 		} catch (Exception e) {
@@ -37,6 +45,20 @@ public class Main {
 		}
 		// 마무리로 entityManger 닫아줘
 		emf.close();
+	}
+
+	private static void printMember(Member member) {
+		String username = member.getUsername();
+		System.out.println("username = " + username);
+	}
+
+	private static void printMemberAndTeam(Member member) {
+		String username = member.getUsername();
+		System.out.println("username = " + username);
+
+		Team team = member.getTeam();
+		System.out.println("team = " + team.getName());
+
 	}
 }
 // 조인전략으로 우선 진행하고 만약 비즈니스 모델이 단순하고 확장할 일이 별로 없을것 같다 하면 단일 테이블 전략 사용
