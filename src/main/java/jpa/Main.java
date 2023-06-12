0dce5744602ff0f54177d85776a9e2b054b44569
naegonggae <tkdtkd97@naver.com>
2023-06-12 14:44:14 +0900

    :memo: 쿼리문 쓸때 주의사항

--- src/main/java/jpa/Main.java
+++ src/main/java/jpa/Main.java
@@ -21,35 +21,9 @@ public class Main {
 		tx.begin(); // 트랜잭션 시작
 
 		try {
-			List<Member> resultList = entityManager.createQuery(
-					// JPQL 은 엔티티를 대상으로 쿼리를 짜고 SQL 은 DB 테이블 대상으로 짠다.
-					// 객체 지향 SQL = JPQL
-					"select m from Member m where m.username like '%KKim%'",
-					Member.class
-			).getResultList();
-
-			for (Member member : resultList) {
-				System.out.println("member = " + member);
-			}
-
-			//Criteria 사용
-			// 자바 코드이기 때문에 컴파일 오류를 제공한다. 대신 복잡하다.
-			// 김영한은 실무에서 안쓴다고함 유지보수가 힘들고 보기 힘들다
-			CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-
-			CriteriaQuery<Member> query = cb.createQuery(Member.class); // Member 에 대한 쿼리를 짤거야
-
-			Root<Member> m = query.from(Member.class);
-
-			CriteriaQuery<Member> cq = query.select(m);
-
-			// 조건에 따라 쿼리를 짜는 동적 쿼리 수행에 있어 유리하다.
-			String username = "sadsf";
-			if (username != null) {
-				cq = cq.where(cb.equal(m.get("username"), "Kim"));
-			}
-			List<Member> list = entityManager.createQuery(cq)
-					.getResultList();
+			// jpql 말고 그냥 쿼리 시작전에는 flush 를 해주자 영속성 컨텍스트에 있는거를 db 에 반영해주고 쿼리를 날려야 하기 때문
+			entityManager.createNativeQuery("select MEMBER_ID, USERNAME, city, street, zipcode from MEMBER")
+							.getResultList();
 
 			tx.commit(); // 트랜잭션 종료 // 임시 저장했던 쿼리를 실제로 날린다.
 		} catch (Exception e) {
